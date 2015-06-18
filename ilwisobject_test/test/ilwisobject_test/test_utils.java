2d5ae6f8188750fca7288188920de4c3d6634ab4
karolyiar <karolyiar@gmail.com>
2015-06-18 02:24:31 +0200

    helloworld test

--- ilwisobject_test/test/ilwisobject_test/test_utils.java
+++ ilwisobject_test/test/ilwisobject_test/test_utils.java
@@ -2,6 +2,9 @@ package ilwisobject_test;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -13,6 +16,7 @@ public class test_utils {
 		final String ilwisLocation = "F:/documents/ilwis/ilwisObjects/output/win32debug/bin/";
 	    try {    	
 	    	System.load(ilwisLocation + "extensions/_ilwisobjects/_ilwisobjects.dll");
+	    	ilwisobjects._initIlwisObjects(ilwisLocation);
 	    } catch (UnsatisfiedLinkError e) {
 	      System.err.println("Native code library failed to load.\n");
 	      e.printStackTrace();
@@ -24,12 +28,19 @@ public class test_utils {
 		}
 	}
 	
+	@AfterClass
+	public static void onceExecutedAfterAll() {
+		ilwisobjects._exitIlwisObjects();
+	}
+	
 	@Before
 	public void setUp() throws Exception {
+		ilwisobjects.disconnectIssueLogger();
+        ilwisobjects.connectIssueLogger();
 	}
 
 	@Test
-	public void color_test() {
+	public void color() {
 		Color c = new Color("RGBA", ilwisobjects.array(new double[]{10, 20, 30, 40}), "blueish");
 		assertEquals("RGBA(10,20,30,40)", c.toString());
 		assertEquals("blueish", c.getName());
@@ -37,7 +48,7 @@ public class test_utils {
 	}
 	
 	@Test
-	public void pixel_test() {
+	public void pixel() {
 		Pixel p = new Pixel(4, 5);
 		assertTrue(p.isValid());
 		assertEquals("pixel(4,5)", p.toString());
@@ -63,7 +74,7 @@ public class test_utils {
 	}
 	
 	@Test
-	public void coordinate_test() {
+	public void coordinate() {
 		Coordinate c = new Coordinate(-1871900.47, 1874237.55);
 		assertEquals(true, c.isValid());
 		assertEquals(false, c.is3D());
@@ -82,8 +93,8 @@ public class test_utils {
 		assertEquals(4.323, c.x(), 0.0001);
 	}
 	
-	@Test
-	public void colorPalette_test() {
+	/*@Test
+	public void colorPalette() {
 		Color col1 = new Color(ColorModel.Value.cmRGBA, ilwisobjects.array(new double[]{220.0, 20.0, 30.0, 200.0}));
 		Color col2 = new Color(ColorModel.Value.cmRGBA, ilwisobjects.array(new double[]{255.0, 80.0, 60.0, 240.0}));
 		Color col3 = new Color(ColorModel.Value.cmRGBA, ilwisobjects.array(new double[]{255.0, 80.0, 69.0, 240.0}));
@@ -105,10 +116,10 @@ public class test_utils {
 		
 		colPal.clear();
 		assertEquals(colPal.valueAt(1, colPal).toString(), colPal.color(1).toString());
-	}
+	}*/
 	
 	@Test
-	public void colorRange_test() {
+	public void colorRange() {
 		Color color1 = new Color(ColorModel.Value.cmRGBA, ilwisobjects.array(new double[]{220.0, 20.0, 30.0, 200.0}));
 		Color color2 = new Color(ColorModel.Value.cmRGBA, ilwisobjects.array(new double[]{255.0, 80.0, 60.0, 240.0}));
 		Color color3 = new Color(ColorModel.Value.cmRGBA, ilwisobjects.array(new double[]{230.0, 60.0, 50.0, 240.0}));
@@ -128,7 +139,7 @@ public class test_utils {
 	}
 	
 	@Test
-	public void numericRange_test() {
+	public void numericRange() {
 		NumericRange nr = new NumericRange(1, 10, 0.2);
 		assertFalse(nr.contains(1.1));
 		assertFalse(nr.contains(0.8));
@@ -143,7 +154,7 @@ public class test_utils {
 	}
 	
 	@Test
-	public void numericItemRange_test() {
+	public void numericItemRange_containment() {
 		NumericItemRange interrange = new NumericItemRange();
 		interrange.add("sealevel", 40, 100);
 		interrange.add("dijks", 101, 151);
@@ -152,10 +163,60 @@ public class test_utils {
 		assertEquals("numericrange:40.0|100.0", interrange.listAll().get(0).get(1));
 		assertEquals("dijks", interrange.listAll().get(1).get(0) );
 		assertEquals("numericrange:101.0|151.0", interrange.listAll().get(1).get(1));
+		
+		ItemDomain childdom = new ItemDomain(interrange);
+		
+		interrange.add("by the sea", 152.0, 181.0, 5.0);
+		ItemDomain parentdom = new ItemDomain(interrange);
+		
+		childdom.setParent( parentdom );
+		
+		assertEquals("cSELF", childdom.contains(40) );
+		assertEquals("cNONE", childdom.contains(39.9) );
+		assertEquals("cNONE", childdom.contains(161.1) );
+		
+		childdom.setStrict(false);
+		assertEquals("cPARENT", childdom.contains(161.1) );
 	}
 	
 	@Test
-	public void thematicRange_test() {
+	public void numericItemRange_parents() {
+		NumericItemRange nir = new NumericItemRange();
+		nir.add("sealevel", 40, 100);
+		ItemDomain childdom = new ItemDomain(nir);
+
+		nir.add("by the sea",  151, 181, 5);
+		ItemDomain parentdom = new ItemDomain(nir);
+		
+		boolean exception = false;
+		try {
+			childdom.parent(); //raises exception
+		} catch (Exception e) {
+			assertEquals("ILWIS ErrorObject: No parent domain found", e.getMessage());
+			exception = true;
+		}
+		assertTrue(exception);
+		
+		childdom.setParent( parentdom );
+		assertTrue( childdom.parent().isValid() );		
+	}
+	
+	@Test
+	public void numericItemRange_removeAndCount() {
+		NumericItemRange nir = new NumericItemRange();
+	    nir.add("sealevel", 40.0, 100.0, 5.0);
+	    nir.add("by the sea", 151.0, 181.0, 5.0);
+	    ItemDomain childdom = new ItemDomain(nir);
+
+	    assertEquals(2, childdom.count());
+	    childdom.removeItem("sealevel");
+	    assertEquals(1, childdom.count());
+	    childdom.add("sealevel");//, 185.0, 250.0, 5.0);
+	    //assertEquals(2, childdom.count());
+	}
+	
+	@Test
+	public void thematicRange() {
 		ThematicRange themRan = new ThematicRange();
 		themRan.add("Sea", "SE", "Area covered by the sea");
 		themRan.add("Sand", "SA", "Area covered by sand");
@@ -163,6 +224,45 @@ public class test_utils {
 		assertTrue(themRan.isValid());
 		assertEquals("Area covered by sand", themRan.listAll().get(1).get(2) );
 	}
+	
+	@Test
+	public void helloWorld() {
+		try {
+			ilwisobjects.disconnectIssueLogger();
+            Engine.setWorkingCatalog("F:/documents/ilwis/helloworld/");
+            ilwisobjects.connectIssueLogger();
+		} catch (Exception e) {
+            fail("could not set working directory!");
+        }            
+		FeatureCoverage world = new FeatureCoverage("countries.mpa");
+		assertTrue( world.isValid() );
+		assertFalse( world.isInternal() );
+		assertEquals( 286, world.featureCount() );
+		
+		HashMap<String, Integer> population_ranking = new HashMap<String, Integer>();
+		
+		for(FeatureIterator country = world.__iter__(); ; country.next()) {
+			try {
+				String name = country.current().attribute("iso_a2", "");
+				if (!population_ranking.containsKey(name))
+					population_ranking.put(name, (int) (country.current().attribute("pop_est", 0)));
+			} catch (Exception e) {
+				break;
+			}
+		}
+		
+		assertEquals(3971020, (int)population_ranking.get("PR"));
+		assertEquals(3418085, (int)population_ranking.get("OM"));
+		assertEquals(14268711, (int)population_ranking.get("MW"));
+	}
+	
+
+	
+	
+	
+	
+	
+	
 	
 
 
