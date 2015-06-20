4374ef91e6246922a9fc9e63346fa2d98bae76c6
karolyiar <karolyiar@gmail.com>
2015-06-21 01:02:55 +0200

    test consts

---
+++ ilwisobject_test/test/ilwisobject_test/TestConst.java
@@ -0,0 +1,46 @@
+package ilwisobject_test;
+
+import static org.junit.Assert.*;
+
+import java.math.BigInteger;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestConst {
+	@BeforeClass
+	public static void onceExecutedBeforeAll() {
+		final String ilwisLocation = TestUtil.ilwisLocation;
+		try {
+			System.load(ilwisLocation
+					+ "extensions/_ilwisobjects/_ilwisobjects.dll");
+			ilwisobjects._initIlwisObjects(ilwisLocation);
+		} catch (UnsatisfiedLinkError e) {
+			System.err.println("Native code library failed to load.\n");
+			e.printStackTrace();
+			System.exit(1);
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@AfterClass
+	public static void onceExecutedAfterAll() {
+		ilwisobjects._exitIlwisObjects();
+	}
+
+	@Test
+	public void undef() {
+		//assertEquals(Const.sUNDEF, "?");
+        //assertEquals(Const.shUNDEF, 32767);
+        assertEquals(-2147483648, ilwisobjects.getIUNDEF());
+        //assertEquals(1e38, ilwisobjects.getFlUNDEF(), 0.00001);
+        assertEquals(-1e308, ilwisobjects.getRUNDEF(), 0.000001);
+        assertEquals(-9223372036854775808L, ilwisobjects.getI64UNDEF());
+        
+	}
+
+}
