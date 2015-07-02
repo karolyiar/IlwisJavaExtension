913a7afb113669b5ea61cdba34278e2d2b04b31f
karolyiar <karolyiar@gmail.com>
2015-07-02 10:26:08 +0200

    Const test

--- ilwisobject_test/test/ilwisobject_test/TestConst.java
+++ ilwisobject_test/test/ilwisobject_test/TestConst.java
@@ -2,7 +2,6 @@ package ilwisobject_test;
 
 import static org.junit.Assert.*;
 
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -12,20 +11,14 @@ public class TestConst {
 		TestUtil.onceExecutedBeforeAll();
 	}
 
-	@AfterClass
-	public static void onceExecutedAfterAll() {
-		TestUtil.onceExecutedAfterAll();
-	}
-
 	@Test
 	public void undef() {
-		//assertEquals(Const.sUNDEF, "?");
-        //assertEquals(Const.shUNDEF, 32767);
+		assertEquals(ilwisobjects.getsUNDEF(), "?");
+        assertEquals(ilwisobjects.getshUNDEF(), 32767);
         assertEquals(2147483645, ilwisobjects.getIUNDEF());
-        //assertEquals(1e38, ilwisobjects.getFlUNDEF(), TestUtil.precision);
+        assertEquals(1e38, ilwisobjects.getFlUNDEF(), 1e33);
         assertEquals(-1e308, ilwisobjects.getRUNDEF(), TestUtil.precision);
         assertEquals(-9223372036854775808L, ilwisobjects.getI64UNDEF());
-        
 	}
 
 }
