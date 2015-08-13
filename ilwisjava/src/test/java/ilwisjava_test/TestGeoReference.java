e23f00523057faa472565e240c6398443e313cf4
karolyiar <karolyiar@gmail.com>
2015-08-13 23:23:24 +0200

    GeoReference test fixed

--- ilwisjava/src/test/java/ilwisjava_test/TestGeoReference.java
+++ ilwisjava/src/test/java/ilwisjava_test/TestGeoReference.java
@@ -31,7 +31,7 @@ public class TestGeoReference {
 	
 	@Test
 	public void fromRaster() {
-		RasterCoverage r = new RasterCoverage("n000302.mpr"); //fails
+		RasterCoverage r = new RasterCoverage("n000302.mpr");
 		GeoReference gr = r.geoReference();
         assertTrue(gr.isValid());
         assertFalse(gr.isInternal());
@@ -39,23 +39,23 @@ public class TestGeoReference {
         assertTrue(r.size().equals( gr.size() ));
         Box box = new Box(gr.size());
         Envelope env = gr.box2Envelope(box);
-        //assertEquals("0 0 0 1151 1151 0", box.toString());
-        //assertEquals("-4.61199e+06 -4.60397e+06 4.60401e+06 4.61203e+06", env.toString());
+        assertEquals("0 0 0 1151 1151 0", box.toString());
+        assertEquals("-4.61199e+06 -4.60397e+06 4.60401e+06 4.61203e+06", env.toString());
         Envelope subenv = new Envelope(new Coordinate(-1e+06, -1e+06), new Coordinate(1e+06, 1e+06));
         Box subbox = gr.envelope2Box(subenv);
         assertEquals("-1e+06 -1e+06 1e+06 1e+06", subenv.toString());
-        //assertEquals("451 451 701 701", subbox.toString());
-        //assertEquals("pixel(536.599,478.436)", gr.coord2Pixel(new Coordinate(-319195.47, 784540.64)).toString());
-        //assertEquals("coordinate(-319198.248000,784544.506500)", gr.pixel2Coord(new PixelD(536.599, 478.436)).toString());
-        //assertEquals("coordinate(-323990.248000,788032.506500)", gr.pixel2Coord(new Pixel(536, 478)).toString());
-        //assertEquals(0, gr.pixelSize(), TestUtil.precision);  // TODO possible bug in GeoRefImplementaion  - nan is not a good pixelSize!!
+        assertEquals("451 451 701 701", subbox.toString());
+        assertEquals("pixel(536.599,478.436)", gr.coord2Pixel(new Coordinate(-319195.47, 784540.64)).toString());
+        assertEquals("coordinate(-319198.248000,784544.506500)", gr.pixel2Coord(new PixelD(536.599, 478.436)).toString());
+        assertEquals("coordinate(-323990.248000,788032.506500)", gr.pixel2Coord(new Pixel(536, 478)).toString());
+//        assertEquals(0, gr.pixelSize(), TestUtil.precision);  // TODO possible bug in GeoRefImplementaion  - nan is not a good pixelSize!!
         assertTrue(gr.centerOfPixel());
-        //assertEquals("536 478 536 478",
-        //		gr.envelope2Box(new Envelope(new Coordinate(-319195.47, 784540.64), new Coordinate(-319990.248000, 784032.506500))).toString());
+        assertEquals("536 478 536 478",
+        		gr.envelope2Box(new Envelope(new Coordinate(-319195.47, 784540.64), new Coordinate(-319990.248000, 784032.506500))).toString());
         gr.setCenterOfPixel(false);
         assertFalse(gr.centerOfPixel());
-        //assertEquals("536 478 536 478",
-        //        gr.envelope2Box(new Envelope(new Coordinate(-319195.47, 784540.64), new Coordinate(-319990.248000, 784032.506500))).toString());
+        assertEquals("536 478 536 478",
+                gr.envelope2Box(new Envelope(new Coordinate(-319195.47, 784540.64), new Coordinate(-319990.248000, 784032.506500))).toString());
         gr.setCenterOfPixel(true);
         assertTrue(gr.centerOfPixel()); 
 	}
